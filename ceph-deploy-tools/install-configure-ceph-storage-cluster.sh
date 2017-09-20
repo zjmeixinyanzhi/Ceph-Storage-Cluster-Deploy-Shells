90b2cd5bea69b3b43e5da842a6f0b05ef2a37a12
zjmeixinyanzhi <zhangjiewanwansui@gmail.com>
2017-09-20 23:28:35 +0800

    set public&storage network setting

--- ceph-deploy-tools/install-configure-ceph-storage-cluster.sh
+++ ceph-deploy-tools/install-configure-ceph-storage-cluster.sh
@@ -24,7 +24,7 @@ do
  echo $name" already is mon!"
  else
  mon_hostname=$mon_hostname","$name
- mon_ip=$mon_ip","$(echo $storage_net|cut -d "." -f1-3).$(echo $ip|awk -F "." '{print $4}')
+ mon_ip=$mon_ip","$ip
  fi
 done;
 echo $osds
@@ -35,10 +35,28 @@ mkdir -p /root/my-cluster
 cd /root/my-cluster
 rm -rf /root/my-cluster/*
 ceph-deploy new $deploy_node
+sed -i -e 's#'"$( cat /root/my-cluster/ceph.conf |grep mon_initial_members)"'#'"$( cat /root/my-cluster/ceph.conf |grep mon_initial_members)$mon_hostname"'#g' /root/my-cluster/ceph.conf
+sed -i -e 's#'"$( cat /root/my-cluster/ceph.conf |grep mon_host )"'#'"$(  cat /root/my-cluster/ceph.conf |grep mon_host )$mon_ip"'#g' /root/my-cluster/ceph.conf
+echo "public network = "$public_net >> /root/my-cluster/ceph.conf
+echo "cluster network = "$storage_net >> /root/my-cluster/ceph.conf
 ceph-deploy install ${nodes_name[@]}
 ceph-deploy mon create-initial
 ceph-deploy osd create $osds
 ceph-deploy admin ${nodes_name[@]}
-sed -i -e 's#'"$( cat /root/my-cluster/ceph.conf |grep mon_initial_members)"'#'"$( cat /root/my-cluster/ceph.conf |grep mon_initial_members)$mon_hostname"'#g' /root/my-cluster/ceph.conf
-sed -i -e 's#'"$( cat /root/my-cluster/ceph.conf |grep mon_host )"'#'"$(  cat /root/my-cluster/ceph.conf |grep mon_host )$mon_ip"'#g' /root/my-cluster/ceph.conf
+### Add mon nodes
+for ((i=0; i<${#monitor_map[@]}; i+=1));
+do
+  name=${monitor_name[$i]};
+  ip=${monitor_map[$name]};
+  echo "Set $name as a ceph monitor" 
+  if [ $name =  $deploy_node ]; then
+    echo $name" already is mon!"
+  else
+    cd /root/my-cluster
+    ceph-deploy mon add $name
+  fi
+done;
+### Update ceph.conf
 ceph-deploy --overwrite-conf config push ${nodes_name[@]}
+ceph -s
+
